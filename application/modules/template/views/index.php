022e406f7685a9101453c61993497a2ccb187a32
Iwan Sunarya <ones006@gmail.com>
2011-06-13 22:01:46 +0700

    remove application

--- application/modules/template/views/index.php
+++
@@ -1,1 +0,0 @@
-<h1>it is just template. for copy paste </h1>
--- application/views/includes/tut_info.php
+++
@@ -1,1 +0,0 @@
-<div id="tutInfo">Tutorial created by <a href="http://net.tutsplus.com">Barry Cogan</a> for <a href="http://net.tutsplus.com">Nettuts+</a></div>
