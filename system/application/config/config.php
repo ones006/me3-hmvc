ec897b4a0958f7c459c7d75b70581a6ee2bf52de
Iwan Sunarya <ones006@gmail.com>
2011-06-15 16:48:26 +0700

    ubah config 2

--- system/application/config/config.php
+++ system/application/config/config.php
@@ -11,7 +11,6 @@
 |	http://www.your-site.com/
 |
 */
-//$config['base_url']	= "http://me3.one/";
 $config['base_url'] = isset($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) == 'on' ? 'https' : 'http';
 $config['base_url'] .= '://'. $_SERVER['HTTP_HOST'];
 $config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
