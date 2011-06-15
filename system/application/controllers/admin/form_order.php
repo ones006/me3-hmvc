44f0f072d5781d56a1ac4d092e560470128a9ae0
Iwan Sunarya <ones006@gmail.com>
2011-06-15 15:39:22 +0700

    update template admin2

---
+++ system/application/controllers/admin/form_order.php
@@ -0,0 +1,22 @@
+<?php
+#doc
+#	classname:	form_order
+#	scope:		admin only
+#
+#/doc
+
+class FormOrder extends Controller
+{
+	#	internal variables
+	
+	#	Constructor
+	function __construct (argument)
+	{
+		# code...
+		
+	}
+	###	
+
+}
+###
+
--- system/application/views/FreakAuth_light/template_admin/footer.php
+++ system/application/views/FreakAuth_light/template_admin/footer.php
@@ -1,5 +1,17 @@
+
 	<div id="footer">
 		<p>Copyright &copy; <?php echo date('Y'); ?> <a href="javascript:;">Metrasat</a>, all rights reserved.</p>	
 	</div> <!-- #footer -->
+<script  type="text/javascript" src="<?=base_url().$this->config->item('FAL_assets_one').'/'.$this->config->item('FAL_css');?>/jquery/jquery.1.4.2.min.js"></script>
+<script  type="text/javascript" src="<?=base_url().$this->config->item('FAL_assets_one').'/'.$this->config->item('FAL_css');?>/slate/slate.js"></script>
+<script  type="text/javascript" src="<?=base_url().$this->config->item('FAL_assets_one').'/'.$this->config->item('FAL_css');?>/slate/slate.portlet.js"></script>
+<script  type="text/javascript" src="<?=base_url().$this->config->item('FAL_assets_one').'/'.$this->config->item('FAL_css');?>/plugin.js"></script>
+<script type="text/javascript" charset="utf-8">
+$(function () 
+{
+	slate.init ();
+	slate.portlet.init ();	
+});
+</script>
 </body>
 </html>
